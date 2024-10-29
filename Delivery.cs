4f967fed990a723112ddc2d7ba9e0e38e67f3636
Bror Lundberg <maximillian.lundber@chasacademy.se>
2024-10-29 11:44:26 +0100

    added 1 abstract parent class for order and 2 child classes with iterface

--- Delivery.cs
+++ Delivery.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ByteBasket
 {
-    internal class Delivery
+    public interface IDelivery
     {
-        public Delivery()
+        void Delivery();
+    }
+    public abstract class Delivery()
+    {
+        public Order Order { get; set; }
+
+        public void Delivery()
         {
-                
+            Console.WriteLine($"the  Order {Order.Id} for {Kund.Name} at {Kund.Adress} is in transit");
         }
     }
+    public class Standard : Delivery, IDelivery 
+    {
+
+    }
+    public class Expres : Delivery, IDelivery
+    { 
+    
+    }
+    
 }
